a77132820a49275a30238c39e7a1cfc7b3f81e45
pakainfo <42144693+pakainfo-com@users.noreply.github.com>
2021-12-07 17:26:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+TikTok Logo Color Hex Code
+The Hex values or Hex Codes of all Colors of TikTok Logo are given below.
+
+Black
+
+Hex Value: #010101;
+
+Light Aqua
+
+Hex Value: #69C9D0;
+
+Red
+
+Hex Value: #EE1D52;
+
+White
+
+Hex Value: #FFFFFF;
+
+TikTok Logo Color RGB Code
+The RGB values or RGB Codes of all Colors of TikTok Logo are given below.
+
+Black
+
+RGB Value: (1, 1, 1)
+
+Light Aqua
+
+RGB Value: (105, 201, 208)
+
+Red
+
+RGB Value: (238, 29, 82)
+
+White
+
+RGB Value: (255, 255, 255)
+
+TikTok Logo Color CMYK Code
+The CMYK values or CMYK Codes of all Colors of TikTok Logo are given below.
+
+Black
+
+CMYK Value: (0%, 0%, 0%, 100%)
+
+//Light Aqua
+
+CMYK Value: (50%, 3%, 0%, 18%)
+
+//Red
+
+CMYK Value: (0%, 88%, 66%, 7%)
+
+//White
+
+CMYK Value: (0%, 0%, 0%, 0%)
